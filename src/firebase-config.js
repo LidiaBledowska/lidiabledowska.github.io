499de174dea0df0bbcbd857a413296fa350ca98d
LidiaBledowska <lilkalilson@gmail.com>
2025-06-21 17:31:14 +0200

    feat: load firebase config from env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyD7ZLyDHFBNsQe9j03YPi0xmdLbqdk_K68",
-    authDomain: "rekrutracker-app.firebaseapp.com",
-    projectId: "rekrutracker-app",
-    storageBucket: "rekrutracker-app.firebasestorage.app",
-    messagingSenderId: "758407291898",
-    appId: "1:758407291898:web:a573e2cd3b416596d37a43",
-    measurementId: "G-HQW1YLG9Q1"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
